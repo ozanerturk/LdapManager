4e0fe99de7a79bc0c8177632d78f1564d9148f78
Ozan ERTÜRK <oerturk@innova.com.tr>
2019-08-09 23:18:33 +0300

    travis yml changed

--- runTests.bat
+++ runTests.bat
@@ -1,4 +1,4 @@
-dotnet tool install dotnet-reportgenerator-globaltool --tool-path tool
+dotnet tool install dotnet-reportgenerator-globaltool --tool-path tools/reportgenerator
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
-"tool/reportgenerator.exe" "-reports:LdapManager.Test/coverage.opencover.xml" "-targetdir:coverage" -reporttypes:Html;HtmlChart
+"tools/reportgenerator/reportgenerator.exe" "-reports:LdapManager.Test/coverage.opencover.xml" "-targetdir:coverage" -reporttypes:Html;HtmlChart
 pause
